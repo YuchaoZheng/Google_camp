6718dfdd7abe710cca6712abc8439e03c6fe28a0
YuchaoZheng <yuchao_zheng@163.com>
2020-01-14 04:20:09 +0000

    update

--- segmentation/make_dataset.py
+++ segmentation/make_dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os 
 
 def make_csv(path, train = False):
     folder_list = os.listdir(path)
@@ -12,7 +13,6 @@ def make_csv(path, train = False):
         folder_list1 = os.listdir(path1)
 
         for f1 in folder_list1:
-
             # clip_00000000
             path2 = os.path.join(path1, f1)
 
@@ -27,7 +27,7 @@ def make_csv(path, train = False):
         result = {"image_name": img_name,
              "train_base_dir": base_dir}
     else:
-        result = {"image_name": img_name,
+        result = {"mask_name": img_name,
                   "test_base_dir": base_dir}
 
     df = pd.DataFrame(result)
@@ -39,7 +39,7 @@ train_dir = '/home/yuchaozheng_zz/ML-camp/MattingHuman/aisegmentcom-matting-huma
 df_img = make_csv(train_dir, True)
 
 test_dir = '/home/yuchaozheng_zz/ML-camp/MattingHuman/aisegmentcom-matting-human-datasets/matting'
-df_mask = make_csv(train_dir, True)
+df_mask = make_csv(test_dir, False)
 
 print('Num Images: ', df_img.shape[0])
 print('Num Masks: ', df_mask.shape[0])
@@ -51,11 +51,12 @@ def get_name(x):
 
 
 df_img['merge_col'] = df_img['image_name'].apply(get_name)
-df_mask['merge_col'] = df_msk['image_name'].apply(get_name)
-
+df_mask['merge_col'] = df_mask['mask_name'].apply(get_name)
+print(len(df_img['merge_col'].unique()))
 df_data = pd.merge(df_img, df_mask, on='merge_col')
 
 # save as a compressed csv file
 df_data.to_csv('df_data.csv', index=False)
-
-
+print(df_data.shape)
+print(df_data.columns)
+print(df_data.head())
