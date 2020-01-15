3575daedd8e4940a858fe51d76f815f5dccfed3d
YuchaoZheng <yuchao_zheng@163.com>
2020-01-15 23:11:08 +0800

    update data

--- segmentation/simple_request.py
+++ segmentation/simple_request.py
@@ -2,6 +2,7 @@ import requests
 import argparse
 from PIL import Image
 import io
+import json
 
 # Initialize the PyTorch REST API endpoint URL.
 PyTorch_REST_API_URL = 'http://35.221.192.94:443/predict'
@@ -11,12 +12,12 @@ def predict_result(image_path):
     # Initialize image path
     image = open(image_path, 'rb').read()
     print(len(image))
-    payload = {'image': image, ''}
+    payload = {'image': image}
 
     dataload = {'smooth': 0.75, 'lip_brighten': 0.3, 'whiten': 0.2, 'sharpen': 0.35, 'thin': 0.8}
 
     # Submit the request.
-    picture_bytes = requests.post(PyTorch_REST_API_URL, data=json.dumps(dataload), files=payload)
+    picture_bytes = requests.post(PyTorch_REST_API_URL, data=dataload, files=payload)
 
     # Ensure the request was successful.
     if picture_bytes != None:
